9800e0655be636eb36461b313cf520e501da16a3
Valerie Tan <1163590061@qq.com>
2018-06-12 23:15:27 +0800

    Update
    GetOrderList&GetOrderDetails实现

--- pages/components/myorder/myorder.js
+++ pages/components/myorder/myorder.js
@@ -1,18 +1,26 @@
 // pages/components/myorder/myorder.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    openid: null,
+    orders: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var arr = new Array()
+    arr = this.GetOrderList()
+    this.setData({
+      orders: arr,
+      openid: app.globalData.openId
+    })
   },
 
   /**
@@ -26,7 +34,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
@@ -62,5 +70,60 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+
+  /* ********************
+  ** 获取订单列表 GetOrderList
+  ** 从后台服务器获取该用户的订单列表
+  ** 参数 : 无
+  ** 返回值 : arr 数组
+  ******************** */
+  GetOrderList: function() {
+    var arr = new Array()
+    var id = app.globalData.openId
+    wx.request({
+      url:'https://private-b4689-ordermeal.apiary-mock.com/eorder/buyer/order/list',
+      data: {
+        'openid': id,
+        'page': 0,
+        'size': 10
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      method: 'GET',
+      success: function(res) {
+        // console.log("orders:", res.data.data)
+        arr = res.data.data
+      }
+    })
+    return arr
+  },
+
+  /* ********************
+  ** 获取订单详情 GetOrderDetails
+  ** 从后台服务器获取该用户的某个订单的详情
+  ** 参数 : openid, orderid
+  ** 返回值 : arr 数组
+  ******************** */
+  GetOrderDetails: function(openid, orderid) {
+    var arr = new Array()
+    wx.request({
+      url:'https://private-b4689-ordermeal.apiary-mock.com/eorder/buyer/order/detail',
+      data:{
+        'openid': openid,
+        'orderid': orderid
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      method: 'GET',
+      success: function(res) {
+        // console.log("order details:", res.data.data)
+        arr = res.data.data
+      }
+    })
+    return arr
+  },
+
 })
